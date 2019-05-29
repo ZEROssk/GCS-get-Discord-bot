d805e2b4665d50c622faa4dd763d62fa687cd96c
zerossk <a.k.jack.ssk@gmail.com>
2019-05-29 16:13:34 +0900

    bot

--- GGCSDB/GGCSDB.go
+++ GGCSDB/GGCSDB.go
@@ -1,30 +1,95 @@
 package main
 
 import (
+	"time"
 	"fmt"
-//	"time"
 	"log"
 	"os"
 	"os/signal"
+	"io/ioutil"
 	"syscall"
 
-	"github.com/ZEROssk/GCS-get-Discord-bot/GGCSDB/Authentication"
-	"github.com/ZEROssk/GCS-get-Discord-bot/GGCSDB/SendMessage"
-	"github.com/ZEROssk/GCS-get-Discord-bot/GGCSDB/SendMessageRegular"
+	"./Authentication"
+	"./Get-Schedule"
+	"github.com/okzk/ticker"
 	"github.com/joho/godotenv"
 	"github.com/bwmarrin/discordgo"
 )
+
+var (
+	get = "!get"
+	set = "!set"
+	man = "!man"
+	manM = "```!get !set !man```"
+
+	RegularTime := 7
+)
+
+func SendM(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	switch {
+	case m.Content == get:
+		schedule := GetSchedule.Get_Sc()
+		s.ChannelMessageSend(m.ChannelID, schedule)
+
+	case m.Content == set:
+		SetChannel(m.ChannelID)
+		s.ChannelMessageSend(m.ChannelID, "チャンネルを設定しました")
+
+	case m.Content == man:
+		s.ChannelMessageSend(m.ChannelID, manM)
+	}
+}
+
+func SetChannel(id string) {
+	file, err := os.OpenFile("ID.txt", os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	file.Write(([]byte)(id))
+}
+
+func SendMRegular(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	schedule := GetSchedule.Get_Sc()
+	Cid := ReadID()
+	if Cid == "" {
+		return
+	}
+
+	ticker := ticker.New(10 * time.Second, func(t time.Time) {
+		s.ChannelMessageSend(Cid, schedule)
+	})
+	fmt.Println(ticker)
+}
+
+func ReadID() string {
+	file, err := os.Open("ID.txt")
+	if err != nil {
+		return ""
+	}
+	defer file.Close()
+
+	id, err := ioutil.ReadAll(file)
+	if err != nil {
+		return ""
+	}
+
+	return string(id)
+}
 
 func Env_load() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-}
-
-func Regular_execution(bot *discordgo.Session) {
-	fmt.Println("check1")
-	bot.AddHandler(SendMessageRegular.SendMRegular)
 }
 
 //func Diff_time() {
@@ -62,8 +127,8 @@ func main() {
 		return
 	}
 
-	bot.AddHandler(SendMessage.SendM)
-	go Regular_execution(bot)
+	bot.AddHandler(SendM)
+	go bot.AddHandler(SendMRegular)
 
 	err = bot.Open()
 	if err != nil {

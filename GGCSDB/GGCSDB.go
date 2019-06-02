b1687f910fceade76cc77cfb1b7e42ea5f37b447
zerossk <a.k.jack.ssk@gmail.com>
2019-06-02 21:56:50 +0900

    ??????????

--- GGCSDB/GGCSDB.go
+++ GGCSDB/GGCSDB.go
@@ -7,27 +7,70 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"regexp"
 
 	"./Authentication"
 	"./Get-Schedule"
-	"github.com/okzk/ticker"
+//	"github.com/okzk/ticker"
 	"github.com/joho/godotenv"
 	"github.com/bwmarrin/discordgo"
 )
 
 var (
-	get = "!get"
 	set = "!set"
+	get = "!get"
+	clear = "!clear"
 	man = "!man"
-	manM = "```!get !set !man```"
+
+	setM = "Set Regular execution"
+	clearM = "Clear Regular execution"
+	manM = "```!get !set !clear !man```"
 
 	Cid string
-	//RegularTime = 7 * time.Second
+	check_num int = 0
+	Rtime time.Duration = 5
+	//kill = make(chan bool)
 )
 
 func SendM(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
+	}
+
+	GetRtime := regexp.MustCompile(`^!set \d\d?`)
+
+	if GetRtime.Match([]byte(m.Content)) {
+		splittedCommand := strings.Split(m.Content, " ")
+
+		now := time.Now().In(jst)
+
+		hour, err := strconv.Atoi(splittedCommand[1])
+		if err != nil {
+			return
+		}
+
+		if 24 <= hour {
+			return
+		}
+
+
+		notificationTime := time.Date(
+			now.Year(),
+			now.Month(),
+			now.Day(),
+			hour,
+			0, // min
+			0, // sec
+			0, // nsec
+			jst,
+		)
+
+		// !(now < notification time)
+		if notificationTime.Before(now) {
+			notificationTime = notificationTime.Add(time.Hour * 24)
+		}
+
+		diff := notificationTime.Sub(now)
 	}
 
 	switch {
@@ -37,7 +80,15 @@ func SendM(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	case m.Content == set:
 		Cid = m.ChannelID
-		s.ChannelMessageSend(m.ChannelID, "チャンネルを設定しました")
+		check_num = 0
+		s.ChannelMessageSend(m.ChannelID, setM)
+
+	case m.Content == clear:
+		check_num = 1
+		Cid = ""
+		Rtime = 0
+		s.ChannelMessageSend(m.ChannelID, clearM)
+		return
 
 	case m.Content == man:
 		s.ChannelMessageSend(m.ChannelID, manM)
@@ -49,14 +100,27 @@ func SendMRegular(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	schedule := GetSchedule.Get_Sc()
-	if Cid == "" {
+	if Cid == "" && Rtime == 5 {
 		return
 	}
 
-	ticker.New(10 * time.Second, func(t time.Time) {
-		s.ChannelMessageSend(Cid, schedule)
-	})
+	schedule := GetSchedule.Get_Sc()
+
+	ticker := time.NewTicker(Rtime * time.Second)
+
+	for {
+		select {
+		case <-ticker.C:
+			if check_num == 1 {
+				ticker.Stop()
+				return
+			}	
+			s.ChannelMessageSend(Cid, schedule)
+		}
+		//ticker := ticker.New(Rtime * time.Second, func(t time.Time) {
+		//	s.ChannelMessageSend(Cid, schedule)
+		//})
+	}
 }
 
 func Env_load() {
@@ -91,9 +155,9 @@ func Env_load() {
 func main() {
 	Authentication.Auth()
 	Env_load()
+	stop := make(chan os.Signal, 1)
 
 	TOKEN := "Bot " + os.Getenv("YOUR_TOKEN")
-	stop := make(chan os.Signal, 1)
 
 	bot, err := discordgo.New(TOKEN)
 	if err != nil {
